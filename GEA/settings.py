189fdc7ff093678f2c102e5c3e5012b2d919920a
Maxi Siracusa <maxivm6@hotmail.com>
2023-04-27 16:27:55 -0300

    db3

--- GEA/settings.py
+++ GEA/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yd0gsej)27qhu89h(!^5x^v+=xeg$^*x9n0ab8mofi#&orl9+6'
-"""SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')"""
+"""SECRET_KEY = 'django-insecure-yd0gsej)27qhu89h(!^5x^v+=xeg$^*x9n0ab8mofi#&orl9+6'"""
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python manage.py migrate && gunicorn GEA.wsgi --log-file -
+web: gunicorn GEA.wsgi --log-file -
