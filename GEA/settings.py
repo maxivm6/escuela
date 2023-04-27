51665868cbaa8647075f63467a173b9956f1e9e8
Maxi Siracusa <maxivm6@hotmail.com>
2023-04-27 11:53:17 -0300

    db2

--- GEA/settings.py
+++ GEA/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yd0gsej)27qhu89h(!^5x^v+=xeg$^*x9n0ab8mofi#&orl9+6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
